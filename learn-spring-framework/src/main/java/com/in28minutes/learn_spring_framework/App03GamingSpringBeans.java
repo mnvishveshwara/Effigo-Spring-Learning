5616676edff35c8a93ace0e9bc6781e699b1147b
Vishveshwara MN <90681681+Vishwamn7@users.noreply.github.com>
2025-01-23 21:34:16 +0530

    Spring learing

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -1,33 +1,23 @@
 package com.in28minutes.learn_spring_framework;
-
-import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration.AnnotationConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.GamingConsole;
-import com.in28minutes.learn_spring_framework.game.MarioGame;
-import com.in28minutes.learn_spring_framework.game.PacmanGame;
-import com.in28minutes.learn_spring_framework.game.SuperContraGame;
+
 
 public class App03GamingSpringBeans {
 
 	public static void main(String[] args) {
 
-		var context=new AnnotationConfigApplicationContext(GamingConfiguration.class);
+		try (var context=
+		new AnnotationConfigApplicationContext(GamingConfiguration.class))
+		{
+			context.getBean(GamingConsole.class).up();
+			context.getBean(GameRunner.class).run();
+		}
 		
-		context.getBean(GamingConsole.class).up();
 		
 		
-////		var game= new MarioGame();
-////		var game= new SuperContraGame();
-//		
-//		var game= new PacmanGame();  // 1. Object Creation
-//		
-//		var gameRunner= new GameRunner(game);  // Object Creation + Wiring of dependencies
-//				// Game is a dependency of GameRunner;
-//		
-//		
-//		gameRunner.run();
 //		
 	}
 
--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/GamingConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/GamingConfiguration.java
@@ -2,6 +2,7 @@ package com.in28minutes.learn_spring_framework;
 
 import org.springframework.context.annotation.Bean;
 
+import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.GamingConsole;
 import com.in28minutes.learn_spring_framework.game.PacmanGame;
 
@@ -12,6 +13,11 @@ public class GamingConfiguration {
 		var game= new PacmanGame();
 		return game;
 	}
-
+	
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner= new GameRunner(game);
+		return gameRunner;
+	}
 	
 }
